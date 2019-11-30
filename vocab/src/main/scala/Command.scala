5dbfc0b2cfa3479f78adef30acff867d63f9778f
Dalton Sweeney <daltyboy11@gmail.com>
2019-11-29 19:34:45 -0800

    almost finished implementation of parseAdd. Only thing left is standardizing hint messages and adding some unit tests

--- vocab/src/main/scala/Command.scala
+++ vocab/src/main/scala/Command.scala
@@ -40,18 +40,43 @@ case object Help                                                                
 case object NoArgs                                                                          extends Command
 case object Version                                                                         extends Command
 
+// Command Error Hints
+sealed trait CommandHint {
+  def message: String
+}
+case class CommandHintInvalidWord(word: String) extends CommandHint {
+  val message = s"$word is not a valid word"
+}
+case class CommandHintInvalidPartOfSpeech(invalid: Invalid) extends CommandHint {
+  val message = s"${invalid.given} is not a valid part of speech"
+}
+
+// Why is my command line parse so outrageously complicated??!?! Because it's purely
+// functional, and (I hope) somewhat extensible.
 object CommandLine {
-  val addArgName                    = "add"
-  val wordsArgName                  = "words"
-  val typeArgName                   = "--type="
-  val modifyArgName                 = "modify"
-  val deleteArgName                 = "delete"
-  val practiceArgName               = "practice"
-  val practiceAllArgName            = "all"
-  val practiceHalfArgName           = "half"
-  val helpArgName                   = "--help"
-  val versionArgName                = "--version"
+  object Args {
+    val addArgName                    = "add"
+    val wordsArgName                  = "words"
+    val modifyArgName                 = "modify"
+    val deleteArgName                 = "delete"
+    val practiceArgName               = "practice"
+    val practiceAllArgName            = "all"
+    val practiceHalfArgName           = "half"
+    val helpArgName                   = "--help"
+    val versionArgName                = "--version"
+    val nounArg                       = "--noun"
+    val verbArg                       = "--verb"
+    val pronounArg                    = "--pronoun"
+    val adjectiveArg                  = "--adjective"
+    val adverbArg                     = "--adverb"
+    val prepositionArg                = "--preposition"
+    val conjunctionArg                = "--conjunction"
+    val interjectionArg               = "--interjectionArg"
+  }
+
   val placeholder                   = "-1"
+
+  import Args._
 
   def parseArgs(args: String): Command = {
     // split by whitespace, remove extra whitespace, and drop the first
@@ -78,16 +103,87 @@ object CommandLine {
     }
 
     argList.head match {
-      case `addArgName` => extractCommand(argList.tail.foldLeft(Right(Add(placeholder, placeholder, None)): Either[Unknown, Add])(parseAdd))
-      case `modifyArgName` => extractCommand(argList.tail.foldLeft(Right(Modify(placeholder, placeholder, None)): Either[Unknown, Modify])(parseModify))
-      case `deleteArgName` => extractCommand(argList.tail.foldLeft(Right(Delete(placeholder, None)): Either[Unknown, Delete])(parseDelete))
-      case `practiceArgName` => extractCommand(argList.tail.foldLeft(Right(Practice(None)): Either[Unknown, Practice])(parsePractice))
+      case `addArgName` =>
+        extractCommand(argList.tail.foldLeft(Right(Add(placeholder, placeholder, None)): Either[Unknown, Add])(parseAdd))
+      case `modifyArgName` =>
+        extractCommand(argList.tail.foldLeft(Right(Modify(placeholder, placeholder, None)): Either[Unknown, Modify])(parseModify))
+      case `deleteArgName` =>
+        extractCommand(argList.tail.foldLeft(Right(Delete(placeholder, None)): Either[Unknown, Delete])(parseDelete))
+      case `practiceArgName` =>
+        extractCommand(argList.tail.foldLeft(Right(Practice(None)): Either[Unknown, Practice])(parsePractice))
     }
   }
+
+  // True if all the characters in `s` are lowercase alphanumeric.
+  // False otherwise.
+  private def containsNonLowercaseAlphanumeric(s: String): Boolean = s.forall(('a' to 'z').contains(_))
+
+  // Converts a string to its corresponding SpeechPart case class/object
+  private def partOfSpeechFromString(s: String): SpeechPart = s match {
+    case `nounArg`                      => Noun
+    case `verbArg`                      => Verb
+    case `pronounArg`                   => Pronoun
+    case `adjectiveArg`                 => Adjective
+    case `adverbArg`                    => Adverb
+    case `prepositionArg`               => Preposition
+    case `conjunctionArg`               => Conjuction
+    case `interjectionArg`              => Interjection
+    case invalid                        => Invalid(invalid)
+  }
   
+  // Assuming `vocab add` prefix, parses `word definition partOfSpeech` or `word partOfSpeech definition`.
+  // If partOfSpeech comes after definition than any input after partOfSpeech is ignored.
   private def parseAdd(tentativeAdd: Either[Unknown, Add], nextArg: String): Either[Unknown, Add] = {
-    // TODO - implement me
-    Left(Unknown(None))
+    tentativeAdd match {
+      case Left(_) => tentativeAdd
+      case Right(add @ Add(word, description, partOfSpeech)) => word match {
+        case `placeholder` if containsNonLowercaseAlphanumeric(nextArg) => Left(Unknown(Some(s"$nextArg not a valid word")))
+        case `placeholder` => Right(Add(nextArg, description, partOfSpeech))
+        
+        // Part of speech comes first
+        case word if partOfSpeech.isEmpty && description == placeholder && nextArg.startsWith("--") =>
+          partOfSpeechFromString(nextArg) match {
+            case Invalid(invalidPartOfSpeech) =>
+              Left(Unknown(Some(s"$invalidPartOfSpeech is not a valid part of speech")))
+            case speechPart => Right(Add(word, description, Some(speechPart)))
+          }      
+
+        // Description comes first
+        case word if partOfSpeech.isEmpty
+        && description == placeholder
+        && !containsNonLowercaseAlphanumeric(nextArg) =>
+          Right(Add(word, nextArg, partOfSpeech))
+
+        // Currently parsing description valid
+        case word if partOfSpeech.isEmpty
+        && description != placeholder
+        && !containsNonLowercaseAlphanumeric(nextArg) =>
+          Right(Add(word, description + nextArg, partOfSpeech))
+
+        // Currently parsing description invalid
+        case word if partOfSpeech.isEmpty
+        && description != placeholder
+        && !containsNonLowercaseAlphanumeric(nextArg)
+        && !nextArg.startsWith("--") =>
+          Left(Unknown(Some(s"${description + nextArg} is not a valid description")))
+
+        // Currently parsing description and hit part of speech
+        case word if partOfSpeech.isEmpty
+        && description != placeholder
+        && nextArg.startsWith("--") =>
+          partOfSpeechFromString(nextArg) match {
+            case Invalid(invalidPartOfSpeech) =>
+              Left(Unknown(Some(s"$invalidPartOfSpeech is not a valid part of speech")))
+            case speechPart => Right(Add(word, description, Some(speechPart)))
+          }
+
+        // Excess input when parsing is finished. just continue along.
+        case word if description != placeholder && !partOfSpeech.isEmpty => Right(add)
+
+        // We should not be here, but just in case, leave an unexpected error unknown
+        case _ => Left(Unknown(Some("An unknown error occured")))
+      }
+    }
   }
 
   private def parseModify(tentativeModify: Either[Unknown, Modify], nextArg: String): Either[Unknown, Modify] = {
