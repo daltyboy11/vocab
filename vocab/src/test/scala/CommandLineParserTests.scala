2db6f5c67ca47516291f1a447508fdb66dfd22ac
Dalton Sweeney <daltyboy11@gmail.com>
2019-11-30 19:04:25 -0800

    add basic tests for help, version, and words

--- vocab/src/test/scala/CommandLineParserTests.scala
+++ vocab/src/test/scala/CommandLineParserTests.scala
@@ -8,4 +8,25 @@ class CommandLineParserTests extends FunSuite {
       CommandLine.parseArgs(args)
     }
   }
+
+  test("vocab help") {
+    val args = "vocab help bla bla bla"
+    assertResult(Right(Help)) {
+      CommandLine.parseArgs(args)
+    }
+  }
+
+  test("vocab version") {
+    val args = "vocab version"
+    assertResult(Right(Version)) {
+      CommandLine.parseArgs(args)
+    }
+  }
+
+  test("vocab words") {
+    val args = "vocab words"
+    assertResult(Right(Words)) {
+      CommandLine.parseArgs(args)
+    }
+  }
 }
