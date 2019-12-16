f62cef7751cebc8d7494d04d3292722216c249e8
Dalton Sweeney <daltyboy11@gmail.com>
2019-12-16 08:37:19 -0800

    test working! turned out to be an issue in formatting the expected string

--- vocab/src/test/scala/commands/WordsCommandTests.scala
+++ vocab/src/test/scala/commands/WordsCommandTests.scala
@@ -1,9 +1,18 @@
 import org.scalatest.funsuite.AnyFunSuite
+import java.io._
+
 import models._
 import storage._
 
 class WordsCommandTests extends AnyFunSuite {
   val projectDir = System.getProperty("user.dir")
+
+  private def writeToFile(s: String, fileName: String): Unit = {
+    val file = new File(s"${projectDir}/src/test/scala/commands/$fileName")
+    val bw = new BufferedWriter(new FileWriter(file))
+    bw.write(s)
+    bw.close()
+  }
 
   def makeStorage = Storage(s"${projectDir}/src/test/scala/commands",
     "words_read_only.csv",
@@ -45,31 +54,30 @@ class WordsCommandTests extends AnyFunSuite {
 
   test("vocab words") {
     implicit val storage = makeStorage
-    val expected = """
-    ------------------------------------------------------------------------------------------
-    | word          | definition                          | part of speech | times practiced |
-    |---------------|-------------------------------------|----------------|-----------------|
-    | ardor         | enthusiasm or passion               | noun           | 0               |
-    |---------------|-------------------------------------|----------------|-----------------|
-    | irascible     | easily angered                      | adjective      | 1               |
-    |---------------|-------------------------------------|----------------|-----------------|
-    | rapacious     | aggresively greedy or grasping      | adjective      | 1               |
-    |---------------|-------------------------------------|----------------|-----------------|
-    | risible       | such as to provoke laughter         | adjective      | 0               |
-    |---------------|-------------------------------------|----------------|-----------------|
-    | peregrination | a long and meandering journey       |                | 3               |
-    |---------------|-------------------------------------|----------------|-----------------|
-    | rapine        | the violent seizure of the property |                | 10              |
-    |               | belonging to someone                |                |                 |
-    |---------------|-------------------------------------|----------------|-----------------|
-    | mellifluous   | sweet or musical sounding           | adjective      | 1               |
-    |---------------|-------------------------------------|----------------|-----------------|
-    | nadir         | the low point of the suffering of   |                | 0               |
-    |               | someone                             |                |                 |
-    ------------------------------------------------------------------------------------------
-    """
+    val expected = List(
+    "------------------------------------------------------------------------------------------",
+    "| word          | definition                          | part of speech | times practiced |",
+    "|---------------|-------------------------------------|----------------|-----------------|",
+    "| ardor         | enthusiasm or passion               | noun           | 0               |",
+    "|---------------|-------------------------------------|----------------|-----------------|",
+    "| irascible     | easily angered                      | adjective      | 1               |",
+    "|---------------|-------------------------------------|----------------|-----------------|",
+    "| rapacious     | aggresively greedy or grasping      | adjective      | 1               |",
+    "|---------------|-------------------------------------|----------------|-----------------|",
+    "| risible       | such as to provoke laughter         | adjective      | 0               |",
+    "|---------------|-------------------------------------|----------------|-----------------|",
+    "| peregrination | a long and meandering journey       |                | 3               |",
+    "|---------------|-------------------------------------|----------------|-----------------|",
+    "| rapine        | the violent seizure of the property |                | 10              |",
+    "|               | belonging to someone                |                |                 |",
+    "|---------------|-------------------------------------|----------------|-----------------|",
+    "| mellifluous   | sweet or musical sounding           | adjective      | 1               |",
+    "|---------------|-------------------------------------|----------------|-----------------|",
+    "| nadir         | the low point of the suffering of   |                | 0               |",
+    "|               | someone                             |                |                 |",
+    "------------------------------------------------------------------------------------------")
     assertResult(expected) {
-      Words.generateTable.mkString("\n") + "\n"
+      Words.generateTable
     }
   }
 }
