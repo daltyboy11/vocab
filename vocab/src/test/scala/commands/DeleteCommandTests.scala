7409bc705ff7f5c727a919a3e6453e51c9bf015c
Dalton Sweeney <daltyboy11@gmail.com>
2019-12-17 09:10:20 -0800

    starting DeleteCommandTests

--- vocab/src/test/scala/commands/DeleteCommandTests.scala
+++ vocab/src/test/scala/commands/DeleteCommandTests.scala
@@ -1,2 +1,27 @@
-class DeleteCommandTests extends BaseCommandTests {
+import org.scalatest.BeforeAndAfter
+import scala.collection.mutable.ListBuffer
+
+class DeleteCommandTests extends BaseCommandTests with BeforeAndAfter {
+  private var copyFiles = new ListBuffer[String]()
+  after {
+    copyFiles foreach deleteFile
+  }
+
+  test("delete existing word") {
+    // TODO
+    val testDeleteWordsCopy = makeFileCopy("test_delete_words.csv")
+    copyFiles += testDeleteWordsCopy
+  }
+
+  test("delete non-existent word") {
+    // TODO
+    val testDeleteWordsCopy = makeFileCopy("test_delete_words.csv")
+    copyFiles += testDeleteWordsCopy
+  }
+
+  test("delete several words") {
+    // TODO
+    val testDeleteWordsCopy = makeFileCopy("test_delete_words.csv")
+    copyFiles += testDeleteWordsCopy
+  }
 }
