0be4268ed05e8f4c2b62c1398a246c69488f826a
Dalton Sweeney <daltyboy11@gmail.com>
2020-04-05 15:59:06 -0400

    remove print statment from main

--- vocab/src/main/scala/Main.scala
+++ vocab/src/main/scala/Main.scala
@@ -4,7 +4,6 @@ import java.nio.file.Paths
 
 object Main {
   def main(args: Array[String]): Unit = {
-    println(Paths.get(".").toAbsolutePath)
     val projectPath = Paths.get(".").toAbsolutePath
     val storagePath = s"$projectPath/src/main/resources/"
     implicit val storage: Storage = Storage(storagePath)
